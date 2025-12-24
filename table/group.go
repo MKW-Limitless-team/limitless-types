8fe07df40049696ffa1c7e55c539855c36bd5cd0
nwoik <nwokoyed@gmail.com>
2025-12-24 14:32:58 +0000

    added sorting

--- table/group.go
+++ table/group.go
@@ -1,5 +1,7 @@
 package table
 
+import "sort"
+
 type Group struct {
 	Name    string    `json:"name,omitempty"`
 	Desc    string    `json:"desc,omitempty"`
@@ -10,4 +12,38 @@ type Group struct {
 
 func (group *Group) AddPlayer(player *Player) {
 	group.Players = append(group.Players, player)
+}
+
+func (group *Group) Total() int {
+	total := 0
+
+	for _, player := range group.Players {
+		total += player.Penalty
+
+		for _, score := range player.Scores {
+			total += score
+		}
+	}
+
+	return total
+}
+
+func SortByTotalScore(groups []*Group) []*Group {
+	sort.Sort(byTotalScore(groups))
+
+	return groups
+}
+
+type byTotalScore []*Group
+
+func (a byTotalScore) Len() int {
+	return len(a)
+}
+
+func (a byTotalScore) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a byTotalScore) Less(i, j int) bool {
+	return a[i].Total() < a[j].Total()
 }
--- table/player.go
+++ table/player.go
@@ -1,8 +1,42 @@
 package table
 
+import "sort"
+
 type Player struct {
 	Name    string `json:"name,omitempty"`
 	Flag    string `json:"flag,omitempty"`
 	Scores  []int  `json:"scores,omitempty"`
 	Penalty int    `json:"penalty,omitempty"`
+}
+
+func (player *Player) Total() int {
+	total := 0
+
+	total += player.Penalty
+
+	for _, score := range player.Scores {
+		total += score
+	}
+
+	return total
+}
+
+func SortByScore(players []*Player) []*Player {
+	sort.Sort(byScore(players))
+
+	return players
+}
+
+type byScore []*Player
+
+func (a byScore) Len() int {
+	return len(a)
+}
+
+func (a byScore) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a byScore) Less(i, j int) bool {
+	return a[i].Total() < a[j].Total()
 }
