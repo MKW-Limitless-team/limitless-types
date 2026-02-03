e9b402cb5ebb2e265a1490c36f6f0ffad59ccca2
nwoik <nwokoyed@gmail.com>
2026-02-03 20:38:03 +0000

    added getFC

--- ltrc/playerdata.go
+++ ltrc/playerdata.go
@@ -1,13 +1,22 @@
 package ltrc
 
 import (
+	"fmt"
 	"sort"
+	"strconv"
+
+	"github.com/MKW-Limitless-team/limitless-types/wwfc"
 )
 
 type PlayerData struct {
 	ProfileID uint64 `json:"profileID,omitempty"`
 	DiscordID string `json:"discordID,omitempty"`
 	Mmr       *int64 `json:"mmr,omitempty"`
+}
+
+func (playerData *PlayerData) GetFC() string {
+	fc := strconv.Itoa(int(wwfc.PidToFC(playerData.ProfileID)))
+	return fmt.Sprintf("%s-%s-%s", fc[:4], fc[4:8], fc[8:12])
 }
 
 type byMmr []*PlayerData
